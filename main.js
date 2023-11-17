fb017d21de0ff011478ff69ca3191478ea761044
nuftillayevjavohir <nuftillayevjavohir@gmail.com>
2023-11-17 16:38:56 +0500

    comment

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 tg = window.Telegram.WebApp
 
+
+const urlParams = new URLSearchParams(window.location.search);
+const param1Value = urlParams.get('param1');
+const param2Value = urlParams.get('param2');
+
+console.log(param1Value, '\n' ,param2Value)
+
 data = 'hello, i array from web app'
 
 btn_send = document.getElementById('send_data')
@@ -21,31 +28,36 @@ btn_main.addEventListener('click', (e) => {
 
 Telegram.WebApp.onEvent("mainButtonClicked", () => {
     tg.sendData(data)
+    console.log(param1Value, '\n' ,param2Value)
 })
 
 
 btn_send.addEventListener('click', (e) => {
     tg.sendData(data)
+    console.log(param1Value, '\n' ,param2Value)
 })
 
 
-// init_data.innerText = `${tg.initDataUnsafe.query_id}\n`
-// init_data.innerText += `${tg.initDataUnsafe.user}\n`
-// init_data.innerText += `${tg.initDataUnsafe.user.id}\n`
-// init_data.innerText += `${tg.initDataUnsafe.user.is_bot}\n`
-// init_data.innerText += `${tg.initDataUnsafe.user.first_name}\n`
-// init_data.innerText += `${tg.initDataUnsafe.user.last_name}\n`
-// init_data.innerText += `${tg.initDataUnsafe.user.username}\n`
-// init_data.innerText += `${tg.initDataUnsafe.user.language_code}\n`
-// init_data.innerText += `${tg.initDataUnsafe.user.is_premium}\n`
-// init_data.innerText += `${tg.initDataUnsafe.user.added_to_attachment_menu}\n`
-// init_data.innerText += `${tg.initDataUnsafe.user.allows_write_to_pm}\n`
-// init_data.innerText += `${tg.initDataUnsafe.user.photo_url}\n`
-// init_data.innerText += `${tg.initDataUnsafe.receiver}\n`
-// init_data.innerText += `${tg.initDataUnsafe.chat}\n`
-// init_data.innerText += `${tg.initDataUnsafe.chat_type}\n`
-// init_data.innerText += `${tg.initDataUnsafe.chat_instance}\n`
-// init_data.innerText += `${tg.initDataUnsafe.start_param}\n`
-// init_data.innerText += `${tg.initDataUnsafe.can_send_after}\n`
-// init_data.innerText += `${tg.initDataUnsafe.auth_date}\n`
-// init_data.innerText += `${tg.initDataUnsafe.hash}\n`
+init_data.innerText = `${tg.initDataUnsafe.query_id}\n`
+init_data.innerText += `${tg.initDataUnsafe.user}\n`
+init_data.innerText += `${tg.initDataUnsafe.user.id}\n`
+init_data.innerText += `${tg.initDataUnsafe.user.is_bot}\n`
+init_data.innerText += `${tg.initDataUnsafe.user.first_name}\n`
+init_data.innerText += `${tg.initDataUnsafe.user.last_name}\n`
+init_data.innerText += `${tg.initDataUnsafe.user.username}\n`
+init_data.innerText += `${tg.initDataUnsafe.user.language_code}\n`
+init_data.innerText += `${tg.initDataUnsafe.user.is_premium}\n`
+init_data.innerText += `${tg.initDataUnsafe.user.added_to_attachment_menu}\n`
+init_data.innerText += `${tg.initDataUnsafe.user.allows_write_to_pm}\n`
+init_data.innerText += `${tg.initDataUnsafe.user.photo_url}\n`
+init_data.innerText += `${tg.initDataUnsafe.receiver}\n`
+init_data.innerText += `${tg.initDataUnsafe.chat}\n`
+init_data.innerText += `${tg.initDataUnsafe.chat_type}\n`
+init_data.innerText += `${tg.initDataUnsafe.chat_instance}\n`
+init_data.innerText += `${tg.initDataUnsafe.start_param}\n`
+init_data.innerText += `${tg.initDataUnsafe.can_send_after}\n`
+init_data.innerText += `${tg.initDataUnsafe.auth_date}\n`
+init_data.innerText += `${tg.initDataUnsafe.hash}\n`
+
+
+
